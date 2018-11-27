bb20ef20474df193c7a15d7348347e37c614794e
Tadeu Mesquita <tadeumesquita@gmail.com>
2018-11-27 10:53:26 -0200

    Mudanças no banco
    Foram inseridos ON DELETE CASCADE para as chaves estrangeiras das tabelas Diciplinas e Plano de Ensino.
    Estas chaves estrangeiras foram modificadas para aceitar valores nulos.

--- tabelas.sql
+++ tabelas.sql
@@ -1,3 +1,67 @@
+-- Table: public.cursos
+
+-- DROP TABLE public.cursos;
+
+CREATE TABLE public.cursos
+(
+  id integer NOT NULL DEFAULT nextval('cursos_id_seq'::regclass),
+  nome character varying(50) NOT NULL,
+  qtde integer NOT NULL,
+  CONSTRAINT pk_c_id PRIMARY KEY (id),
+  CONSTRAINT uk_c_nome UNIQUE (nome)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.cursos
+  OWNER TO postgres;
+
+
+-- Table: public.disciplinas
+
+-- DROP TABLE public.disciplinas;
+
+CREATE TABLE public.disciplinas
+(
+  id integer NOT NULL DEFAULT nextval('disciplinas_id_seq'::regclass),
+  fk_curso integer DEFAULT nextval('disciplinas_fk_curso_seq'::regclass),
+  nome character varying(50) NOT NULL,
+  semestre integer NOT NULL,
+  CONSTRAINT pk_d_id PRIMARY KEY (id),
+  CONSTRAINT fk_d_fk_curso FOREIGN KEY (fk_curso)
+      REFERENCES public.cursos (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.disciplinas
+  OWNER TO postgres;
+
+
+-- Table: public.planos_ensino
+
+-- DROP TABLE public.planos_ensino;
+
+CREATE TABLE public.planos_ensino
+(
+  id integer NOT NULL DEFAULT nextval('planos_ensino_id_seq'::regclass),
+  fk_disciplina integer DEFAULT nextval('planos_ensino_fk_disciplina_seq'::regclass),
+  objetivo character varying(50),
+  conteudo text,
+  CONSTRAINT pk_pe_id PRIMARY KEY (id),
+  CONSTRAINT fk_pe_fk_disciplina FOREIGN KEY (fk_disciplina)
+      REFERENCES public.disciplinas (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.planos_ensino
+  OWNER TO postgres;
+
+/*
+
 -- Tabela: Curso
 CREATE TABLE cursos (
   id   SERIAL,
@@ -26,3 +90,4 @@ CREATE TABLE planos_ensino (
   CONSTRAINT pk_pe_id PRIMARY KEY (id),
   CONSTRAINT fk_pe_fk_disciplina FOREIGN KEY (fk_disciplina) REFERENCES disciplinas (id)
 );
+*/
